cc7025498fd071a3af31f1c82ea7c1976f57942b
Tom Scallon <tssnowguy13@gmail.com>
2015-03-03 00:36:01 -0600

    Update web files to support switching of sections and different color schemes

--- public/script.js
+++ public/script.js
@@ -34,18 +34,38 @@
     "Nav in footer": "<= 630"
   }));
 
-  var nav, header, footer;
+  var nav, 
+      header,
+      currentSection = 0,
+      links,
+      sections,
+      footer;
+
+  var select = function( index ) {
+    var link;
+
+    // deselect the previous section
+    (link = links[ currentSection ]).classList.remove("active");
+    sections[ currentSection ].classList.remove("active");
+    document.body.classList.remove( link.innerHTML.toLowerCase() );
+    
+    // now select the next one
+    currentSection = index;
+    (link = links[ currentSection ]).classList.add("active");
+    sections[ currentSection ].classList.add("active");
+    document.body.classList.add( link.innerHTML.toLowerCase() );
+  };
 
   var resize = function() {
-    var ww = window.innerWidth;
-    
     // update width-related constraints
-    Constraints.update( ww );
+    Constraints.update( window.innerWidth );
     
-    // remove body classes, detach <nav>
+    // remove body classes, detach <nav> (in preparation for size-based stuff)
     document.body.className = "";
+    document.body.style.height = window.innerHeight + "px";
     nav.remove();
     
+    // update stuff related to constraints
     if( Constraints.get("Nav in footer") ) {
       document.body.classList.add("nav-in-footer");
       footer.appendChild( nav );
@@ -55,16 +75,32 @@
   }
 
   window.onload = function() {
-    var forEach = Array.prototype.forEach;
-
+    var forEach = Array.prototype.forEach,
+        filter  = Array.prototype.filter,
+        indexOf = Array.prototype.indexOf,
+        isElement = function( node ) { return "tagName" in node; };
+    
+    // get references to common elements
     nav = document.getElementById("nav");
     header = document.getElementById("header");
     footer = document.getElementById("footer");
 
-    /*forEach.call( nav.childNodes, function( node ) {
-      node.id = "nav-" + node.innerHTML;
-    });*/
+    // get the links and sections, then select the first of each
+    links = filter.call( nav.childNodes, isElement );
+    sections = filter.call( document.getElementById("main").childNodes, isElement );
+
+    // when nav is clicked, update selection if necessary
+    nav.onclick = function( ev ) {
+      var index = indexOf.call( links, ev.target );
+      
+      // only select the index if it's not -1
+      index === -1 || select( index );
+    };
+
+    // make the first section active
+    setTimeout( function() { select( 0 ) }, 1 );
     
+    // initialize size-based necessities
     resize();
   }
 
