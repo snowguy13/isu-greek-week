c280a9a2efa27c62c2b407abbe302c543ed71540
Tom Scallon <tssnowguy13@gmail.com>
2016-01-16 22:09:51 -0600

    Tweet scrolling and transitions now work\!\!\!

--- webapp/js/comp/tweetBar.js
+++ webapp/js/comp/tweetBar.js
@@ -18,12 +18,18 @@ var getTweetBar,
 
     // The interval to space Tweet update requests
     REFRESH_RATE = 60000,
+    
+    // The animation duration for sliding Tweets in and out
+    SLIDE_DURATION = 500,
 
     // The duration between the time Tweet is first shown and begins scrolling
-    TWEET_START_SCROLL = 2000,
+    BEFORE_SCROLL = 2000,
+
+    // The rate at which extra Tweet width is scrolled through (in px/s)
+    SCROLL_RATE = 100,
 
     // The duration between the time a Tweet stops scrolling and is hidden 
-    TWEET_HIDE = 1000,
+    BEFORE_SWAP = 1000,
 
     activateTweetBarOnTweetsLoaded = false,
 
@@ -63,7 +69,7 @@ setInterval( requestTweets, REFRESH_RATE );
 
 // Turns Tweet JSON into HTML element
 tweetToHTML = function( tweetData ) {
-  return $("<div class='tweet'></div>").text( tweetData.text );
+  return $("<span class='tweet'></span>").html( tweetData.text );
 };
 
 // Assumes Tweets have been loaded and DOM is ready
@@ -74,22 +80,76 @@ activateTweetBar = function( bar ) {
       // Where Tweets are held
       container = bar.find(".tweet-container"),
       
-      // Ushers in the next Tweet
+      scrollTweet,
       nextTweet;
 
+  scrollTweet = function() {
+    // Determine if scrolling is necessary
+    var tw = tweetEl.outerWidth(),
+        cw = container.outerWidth(),
+        diff;
+    
+    if( tw > cw ) {
+      // Scrolling is necessary
+      diff = tw - cw;
+
+      // Animate to show the whole Tweet
+      tweetEl.animate({
+        left: -diff + "px"
+      }, 1000 * diff / SCROLL_RATE, function() {
+        // Once finished, wait until a swap should occur
+        setTimeout( nextTweet, BEFORE_SWAP );
+      });
+    } else {
+      // No scrolling needed
+      setTimeout( nextTweet, BEFORE_SWAP );
+    }
+  };
+  
+  // Ushers in the next Tweet
   nextTweet = function() {
+    console.log("Showing next Tweet");
+    var oldTweetEl = tweetEl;
+
     // Determine the next index, create the element
     index   = ( index + 1 ) % tweets.length;
     tweetEl = tweetToHTML( tweets[ index ] );
 
-    // Add the Tweet to the container
-    container
-      .empty()
-      .append( tweetEl );
+    // Start the new Tweet off the bottom of the container
+    tweetEl.css({
+      top:     "100%",
+      opacity: 0
+    });
+
+    container.append( tweetEl );
+
+    // Animate the old Tweet out (if there was an old one)
+    if( oldTweetEl != null ) {
+      oldTweetEl.animate({
+        bottom:  "100%",
+        opacity: 0
+      }, SLIDE_DURATION, function() {
+        // Once the animation is done, remove the Tweet
+        oldTweetEl.remove();
+      });
+    }
+
+    // Animate the new Tweet in
+    tweetEl.animate({
+      top:     "0px",
+      opacity: 1
+    }, SLIDE_DURATION, function() {
+      // Remove any styling of the 'top' property
+      tweetEl.css("top", "");
+
+      // Wait the necessary time to begin scrolling
+      setTimeout( scrollTweet, BEFORE_SCROLL );
+    });
   };
 
   // Switch Tweets according to the time properties
-  setInterval( nextTweet, 3000 );
+  //setInterval( nextTweet, 3000 );
+  nextTweet();
 };
 
 return function() {
