4585bc0e77412fd9aaefe201e85f354c65b5e68b
Tom Scallon <tssnowguy13@gmail.com>
2016-02-11 17:49:03 -0600

    Add team <div> generation

--- webapp/js/comp/sections/teams.js
+++ webapp/js/comp/sections/teams.js
@@ -1,8 +1,36 @@
-define(["jquery"],
-function( $ ) {
+define(["jquery", "data/teams"],
+function( $,       teams ) {
+
+var createElementForTeam = function( team ) {
+  var container = $("<div />").addClass("team"),
+      name      = $("<div />").addClass("name"),
+      chapters  = $("<ul />").addClass("chapter-list");
+
+  // Add the name to its element
+  name.html( team.name );
+
+  // Add the chapters to their element
+  team.chapters.forEach(function( chapter ) {
+    var el = $("<li />")
+      .addClass("chapter")
+      .text( chapter );
+    
+    // Append the element
+    chapters.append( el );
+  });
+
+  // Add the name and chapter to the container
+  container.append([ name, chapters ]);
+};
 
 var prepare = function( el, done ) {
-  
+  // Add a container for each team
+  teams.forEach(function( team ) {
+    el.append( createElementForTeam( team ) );
+  });
+
+  // That's it!
+  done();
 };
 
 return {
