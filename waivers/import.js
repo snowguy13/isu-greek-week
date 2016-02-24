5a0cdbe731c4382e47112e00e94e0ad7e5b16c05
Tom Scallon <tssnowguy13@gmail.com>
2016-02-23 18:46:39 -0600

    Minor modifications to import.js, rename waivers.xls to waivers-lipsync.xls

--- waivers/import.js
+++ waivers/import.js
@@ -1,2 +1,7 @@
 var xlsx = require("xlsx");
 
+// A list of workbooks to read in
+// If <name> is an element of BOOKS, then the script will look for a book
+// called waivers-<name>.xls and will import the waiver data into a table
+// called waivers.<name>
+var BOOKS = ["lipsync"];
