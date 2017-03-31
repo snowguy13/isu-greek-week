01365b1b1a6a7c87246cf438474776f610e407d6
Tom Scallon <tssnowguy13@gmail.com>
2017-03-31 15:35:59 -0500

    Re-enable forcing of lip sync waivers. Last year, general waivers sufficed for everything so we only forced general waivers.

--- checkin/webpage/script.js
+++ checkin/webpage/script.js
@@ -82,7 +82,7 @@ var updateResultActions = function() {
       message.html("Isn't " + person.first + " supposed to be <strong>disaffiliated</strong>...?");
       person.valid = false;
       person.canForce = false;
-    } else if( event.waiver && (!person["w_" + event.waiver] && !person.w_general) ) {
+    } else if( event.waiver && (!person["w_" + event.waiver]/* && !person.w_general*/) ) {
       // Otherwise, if there is an event and the member doesn't have the required waiver, note that
       message.html("In order to check in, " + person.first + " must fill out a <strong>" + event.waiver + "</strong> waiver.<br />Press [Control] + [Enter] to force a check-in (this will also mark " + person.first + " as having filled out a waiver).");
       person.valid = false;
@@ -434,7 +434,7 @@ elem.checkin.result.mousedown(function( ev ) {
 }).keypress(function( ev ) {
   var key = ev.keyCode || ev.which,
       p = results[ selected ],
-      e = event
+      e = event,
       t = $(this);
 
   // Ignore key events if currently checking in a member
@@ -459,7 +459,7 @@ elem.checkin.result.mousedown(function( ev ) {
         data: JSON.stringify({
           id:    p.id,
           event: e.name,
-          force: p.valid ? false : /*e.waiver*/ "general"
+          force: p.valid ? false : e.waiver /*"general"*/
         }),
 
         success: function( data ) {
